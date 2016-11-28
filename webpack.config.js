ff134325cc41723c199ea9eeb7cc261eeef24672
Ingo Wiarda <iwiarda@connexity.com>
2016-11-28 16:06:24 +0100

    Add node.js package info and dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// var webpack = require('webpack')
+var path = require('path')
+
+var BUILD_DIR = path.resolve(__dirname+'/build')
+var APP_DIR = path.resolve(__dirname+'/app')
+var webpack = require('webpack')
+
+var config = {
+    entry: APP_DIR+'/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: "bundle.js"
+    },
+    devtool: 'source-map',
+    devServer:{
+	inline:true,
+	contentBase: BUILD_DIR,
+	port:3030
+    },
+    module: {
+        loaders: [
+	    {
+		test: /\.(js|jsx)$/,
+		include: APP_DIR,
+ 		loader: 'babel-loader'
+	    } 
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+
+ };
+
+module.exports = config
