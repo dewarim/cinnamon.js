4fe5b9fcaf3de2e2c8cc57817ae656b8f7eb0c3f
ingo.wiarda <ingo_wiarda@dewarim.de>
2017-04-17 15:23:14 +0200

    whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 // var webpack = require('webpack')
 var path = require('path')
 
-var BUILD_DIR = path.resolve(__dirname+'/build')
-var APP_DIR = path.resolve(__dirname+'/app')
+var BUILD_DIR = path.resolve(__dirname + '/build')
+var APP_DIR = path.resolve(__dirname + '/app')
 var webpack = require('webpack')
 
 var config = {
-    entry: APP_DIR+'/index.jsx',
+    entry: APP_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
         filename: "bundle.js"
     },
     devtool: 'source-map',
-    devServer:{
-	inline:true,
-	contentBase: BUILD_DIR,
-	port:3030
+    devServer: {
+        inline: true,
+        contentBase: BUILD_DIR,
+        port: 3030
     },
     module: {
         loaders: [
-	    {
-		test: /\.(js|jsx)$/,
-		include: APP_DIR,
- 		loader: 'babel-loader'
-	    } 
+            {
+                test: /\.(js|jsx)$/,
+                include: APP_DIR,
+                loader: 'babel-loader'
+            }
         ]
     },
     plugins: [
@@ -33,6 +33,6 @@ var config = {
         })
     ]
 
- };
+};
 
 module.exports = config
